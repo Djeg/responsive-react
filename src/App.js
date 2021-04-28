31ca003fbdeeecdc093d0606bbda005eec15e9c8
David Jegat <david.jegat@knplabs.com>
2021-04-28 17:03:48 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import classNames from './App.module.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classNames.App}>
+      <header className={classNames.header}>
+        <div className={classNames.burger} onClick={toggleMenu}>
+          <i className="fas fa-bars"></i>
+        </div>
+        <div className={classNames.logo}>
+          <img src="https://loremflickr.com/320/240" alt="logo" />
+        </div>
+        <ul className={classNames.tabletMenu}>
+          <li>
+            <a href="#">Menu 1</a>
+          </li>
+          <li>
+            <a href="#">Menu 2</a>
+          </li>
+          <li>
+            <a href="#">Menu 3</a>
+          </li>
+          <li>
+            <a href="#">Menu 4</a>
+          </li>
+          <li>
+            <a href="#">Menu 5</a>
+          </li>
+        </ul>
       </header>
+      <ul className={`${classNames.menu} ${menuOpen ? classNames.active : ''}`}>
+        <li>
+          <a href="#">Menu 1</a>
+        </li>
+        <li>
+          <a href="#">Menu 2</a>
+        </li>
+        <li>
+          <a href="#">Menu 3</a>
+        </li>
+        <li>
+          <a href="#">Menu 4</a>
+        </li>
+        <li>
+          <a href="#">Menu 5</a>
+        </li>
+      </ul>
+
+      <h1>Hello</h1>
     </div>
   );
 }
